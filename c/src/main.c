42669e0d4aae58275ea389bd73e055319cb5bec0
karldmenzel <k.menzel15@gmail.com>
2025-03-08 11:17:20 -0600

    c: add guide on pointers

--- c/src/main.c
+++ c/src/main.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 #include "my_functions.h"
 
+void pointer_guide() {
+
+	printf("Welcome to the guide on pointers!\n");
+
+	int myVariable = 500;
+
+	printf("The value of myVariable is: %d\n", myVariable);
+
+	printf("The memory address of myVariable is: %p\n", &myVariable);
+
+	int *pointerToMyVariable = &myVariable;
+
+	printf("The value of pointerToMyVariable is: %p\n", pointerToMyVariable);
+
+	printf("The value of the memory at the address stored in pointerToMyVariable is: %d\n", *pointerToMyVariable);
+
+	printf("The memory address of pointerToMyVariable is: %p\n", &pointerToMyVariable);
+
+	int **pointerPointer = &pointerToMyVariable;
+
+	printf("The value of pointerPointer is: %p\n", pointerPointer);
+
+	printf("The single dereference of pointerPointer is: %p\n", *pointerPointer);
+
+	printf("The double dereference of pointerPointer is: %d\n", **pointerPointer);
+
+}
+
 int main() {
-	printf("Hello world from the C language!\n");
+	printf("Hello world from the C language!\n\n");
 
-	int i = my_add(1, 2);
+	int i = my_add(1, 3);
 
-	printf("My adding function result: %d\n", i);	
+	printf("My adding function result: %d\n\n", i);
+
+	pointer_guide();
 
 	return 0;
 }
